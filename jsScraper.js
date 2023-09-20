2c76a54e7f1af139d9a00bf2bfc71d731f64517b
awdem <wdemontmollin@gmail.com>
2023-09-20 17:12:39 +0100

    adds main function skeleton, and CSS selectors

--- jsScraper.js
+++ jsScraper.js
@@ -4,6 +4,12 @@ const fs = require('fs')
 const start_url = 'http://quotes.toscrape.com/js/'
 //const start_url = 'http://quotes.toscrape.com/js-delayed/'
 
+// CSS selectors
+const quote_elem_selector = '.quote'
+const quote_text_selector = '.text'
+const quote_author_selector = '.author'
+const quote_tag_selector = '.tag'
+const next_page_selector = '.next > a'
 
 const quotes_list = []
 
@@ -17,3 +23,17 @@ const prepare_browser = async () => {
 	return browser;
 }
 
+const main = async () => {
+	const browser = await prepare_browser()
+	// opens a page in the browser
+	const page = await browser.newPage()
+	// Navigates to the start_url and does the scraping
+	await get_page(page, start_url)
+	// closes the browser
+	await browser.close()
+	console.log(quotes_list)
+}
+
+
+
+// main()
